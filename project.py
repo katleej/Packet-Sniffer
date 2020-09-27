0448290683fd97502074eb94035f1cfeac36d9a1
Katniss Lee <kat@Katnisss-MacBook-Pro.local>
2020-09-27 16:00:27 +0900

    now implemented DNS

--- project.py
+++ project.py
@@ -1,8 +1,6 @@
 import socket
 from struct import *
-from binascii import *
 import pcapy
-import re
 
 def main():
 
@@ -16,120 +14,105 @@ def main():
 	device = devices[int(index)]
 	
 	#tell user which device was chosen
-	print("You are sniffing device " + device)
-
+	print("Device chosen: " + device)
 	#this value will be used to filter out between http and dns
 	selected = int(input("Which header do you want to sniff? Enter the number (HTTP=1, DNS=2): "))
 	print("Sniffing...")
 
 	cap = pcapy.open_live(device , 65535 , 1 , 0)
 
-	#counter
+	#this value will be used at the beginning of each packet to keep track of total number of packets being sniffed
 	num = 1
 
-	#start sniffing packets
+	#start sniffing packets by calling sniff_packet(packet, numbering packet, http or dns)
 	while(num) :
-		(header, packet) = cap.next()
+		header, packet = cap.next()
 		num = sniff_packet(packet, num, selected)
 
-#Convert a string of 6 characters of ethernet address into a dash separated hex string
-def eth_addr (a) :
-	b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]) , ord(a[1]) , ord(a[2]), ord(a[3]), ord(a[4]) , ord(a[5]))
-	return b
-
-#function to parse a packet
 def sniff_packet(packet, num, selected) :
-	
-	#parse ethernet header
+
 	eth_length = 14
 	
-	eth_header = packet[:eth_length]
-	eth = unpack('!6s6sH' , eth_header)
-	eth_protocol = socket.ntohs(eth[2])
+	#we are going to look at the first 14 bytes to get dest, source, type(protocol)
+	dest, source, protocol = unpack('!6s6sH' , packet[:14])
+	eth_protocol = socket.htons(protocol)
 
 	#Parse IP packets, IP Protocol number = 8
 	if eth_protocol == 8 :
 		#Parse IP header
-		#take first 20 characters for the ip header
+		#take first 20 bytes for the ip header because we don't need the rest
 		ip_header = packet[eth_length:20+eth_length]
-		
-		#now unpack them :)
 		iph = unpack('!BBHHHBBH4s4s' , ip_header)
 
+		#get version and IHL that each take 2 bits 
 		version_ihl = iph[0]
 		version = version_ihl >> 4
-		ihl = version_ihl & 0xF
+		ihl = version_ihl & 15
 
+		#entire length of header is kept to trim correctly for next step
 		iph_length = ihl * 4
 
 		protocol = iph[6]
-		s_addr = socket.inet_ntoa(iph[8]);
-		d_addr = socket.inet_ntoa(iph[9]);
+		source_address = socket.inet_ntoa(iph[8]);
+		destination_address = socket.inet_ntoa(iph[9]);
 
 		#TCP protocol
 		if protocol == 6 :
 			t = iph_length + eth_length
 			tcp_header = packet[t:t+20]
 
-			#now unpack them :)
 			tcph = unpack('!HHLLBBHHH' , tcp_header)
-			
 			source_port = tcph[0]
-			dest_port = tcph[1]
-			sequence = tcph[2]
-			acknowledgement = tcph[3]
-			doff_reserved = tcph[4]
-			tcph_length = doff_reserved >> 4
+			destination_port = tcph[1]
+			offset = tcph[4]
+			tcph_length = offset >> 4 
 
+			#case when user selected HTTP option
 			if selected == 1:
-				#case is HTTP
 
 				#handle http request (http request is when destination is 80)
-				if (str(dest_port) == "80"):
+				if (str(destination_port) == "80"):
 
-					h_size = eth_length + iph_length + tcph_length * 4
-					
-					#get remaining values of packet
-					data = packet[h_size:]
-					headers_text = data.partition(b'\r\n\r\n')[0].decode('utf-8', errors='ignore')
+					total_length = eth_length + iph_length + tcph_length * 4
+					data = packet[total_length:]
+					headers = data.partition(b'\r\n\r\n')[0].decode('utf-8', errors='ignore')
 
 					#filter out invalid data files
-					if "HTTP" in headers_text:
-						print(str(num) + ' ' + str(s_addr) + ':' + str(source_port) + ' ' + str(d_addr) + ':' + str(dest_port) + ' HTTP REQUEST')
+					if "HTTP" in headers:
+						print(str(num) + ' ' + str(source_address) + ':' + str(source_port) + ' ' + str(destination_address) + ':' + str(destination_port) + ' HTTP REQUEST')
 						num += 1
-						print(headers_text)
+						print(headers)
 						print('\n')
 
 				#handle http response (http response is when source is 80)
 				if (str(source_port) == "80"):
 				
-					h_size = eth_length + iph_length + tcph_length * 4
-					
-					#get remaining values of packet
-					data = packet[h_size:]
-					headers_text = data.partition(b'\r\n\r\n')[0].decode('utf-8', errors='ignore')
+					total_length = eth_length + iph_length + tcph_length * 4
+					data = packet[total_length:]
+					headers = data.partition(b'\r\n\r\n')[0].decode('utf-8', errors='ignore')
 
 					#filter out invalid data files: 
-					if "HTTP" in headers_text: 
-						print(str(num) + ' ' + str(s_addr) + ':' + str(source_port) + ' ' + str(d_addr) + ':' + str(dest_port) + ' HTTP RESPONSE')
+					if "HTTP" in headers: 
+						print(str(num) + ' ' + str(source_address) + ':' + str(source_port) + ' ' + str(destination_address) + ':' + str(destination_port) + ' HTTP RESPONSE')
 						num += 1
-						print(headers_text)
+						print(headers)
 						print('\n')
 
+			#case when user selected DNS option
 			else:
-				if (str(dest_port) == "53" or str(source_port) == '53'):
-					h_size = eth_length + iph_length + tcph_length * 4
-					
-					#get remaining values of packet
-					data = packet[h_size:]
-					headers_text = data.partition(b'\r\n\r\n')[0].decode('utf-8', errors='ignore')
+				if (str(destination_port) == "53" or str(source_port) == '53'):
+					total_length = eth_length + iph_length + tcph_length * 4
+					data = packet[total_length:]
+					headers = data.partition(b'\r\n\r\n')[0].decode('utf-8', errors='ignore')
 
 					#filter out invalid data files: 
-					print(str(num) + ' ' + str(s_addr) + ':' + str(source_port) + ' ' + str(d_addr) + ':' + str(dest_port) + ' HTTP RESPONSE')
+					print(str(num) + ' ' + str(source_address) + ':' + str(source_port) + ' ' + str(destination_address) + ':' + str(destination_port) + ' DNS')
 					num += 1
 					print(headers_text)
 					print('\n')
 
+
+		#UDP protocol
 		if protocol == 17 and selected == 2:
 			u = iph_length + eth_length
 			udph_length = 8
@@ -139,20 +122,34 @@ def sniff_packet(packet, num, selected) :
 			udph = unpack('!HHHH' , udp_header)
 			
 			source_port = udph[0]
-			dest_port = udph[1]
+			destination_port = udph[1]
 			length = udph[2]
 			checksum = udph[3]
-			
-			print(str(num) + ' ' + str(s_addr) + ':' + str(source_port) + ' ' + str(d_addr) + ':' + str(dest_port))
-			
-			h_size = eth_length + iph_length + udph_length
-			data_size = len(packet) - h_size
-			
-			#get data from the packet
-			data = packet[h_size:]
-			#headers_text = data.partition('ID')[0].decode('utf-8')
-			#print('Data : ' + headers_text)
 
+			if (str(destination_port) == "53" or str(source_port) == '53'):
+				h_size = eth_length + iph_length + udph_length
+				data_size = len(packet) - h_size
+				dns_data = packet[h_size:h_size+12]
+				dnsh = unpack('!HHHHHH', dns_data)
+
+				#bit operation for the multi-bit values
+				middle_values = dnsh[1]
+				QR = str(middle_values >> 15)
+				Opcode = format((middle_values >> 11) & 15, '04b')
+				AA = str((middle_values >> 10) & 1)
+				TC = str((middle_values >> 9) & 1)
+				RD = str((middle_values >> 8) & 1)
+				RA = str((middle_values >> 7) & 1)
+				Z = format((middle_values >> 4) & 7, '03b')
+				RCODE = format(middle_values & 15, '04b')
+				print(str(num) + ' ' + str(source_address) + ':' + str(source_port) + ' ' + str(destination_address) + ':' + str(destination_port) + '  DNS ID: ' + hex(dnsh[0]))
+				print(QR + ' | ' + Opcode + ' | ' + AA + ' | ' + TC + ' | ' + RD + ' | ' + RA + ' | ' + Z + ' | ' + RCODE)
+				print('QDCOUNT: ' + str(dnsh[2]))
+				print('ANCOUNT: ' + str(dnsh[3]))
+				print('NSCOUNT: ' + str(dnsh[4]))
+				print('ARCOUNT: ' + str(dnsh[5]))
+
+	#return this number to update the num
 	return num	
 
 main()
